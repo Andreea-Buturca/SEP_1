c5f57f01c10948c4d19bad1e0368b65ec6dfdbc2
Marek <marek.lewy23@gmail.com>
2016-12-04 14:06:48 +0100

    first version of tripconstruct

--- src/main/Model/Trip.java
+++ src/main/Model/Trip.java
@@ -1,5 +1,6 @@
 package main.Model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,39 +9,49 @@ import java.util.Date;
  */
 public class Trip {
 
-    private ArrayList<Customer> customers = new ArrayList<>();
+
 
     private Bus bus;
     private Chauffeur chauffeur;
     private Destination pickUpPoint;
     private Destination destination;
-    private ArrayList<Destination> stops;
+    private DestinationList stops;
     private Date timeStart;
     private Date timeEnd;
-    private ArrayList<String> notes;  // TODO: 29-Nov-16 so do we need it?
+    private ArrayList<String> notes; // TODO: 29-Nov-16 so do we need it?
+    private Customer customer;
+    private boolean isPrivate;
+    private int distance;
+    private int price;
 
-    public Trip(Bus bus, Chauffeur chauffeur, Destination pickUpPoint, Destination destination, ArrayList<Destination> stops, Date timeStart, Date timeEnd) {
-        this.bus = bus;
-        this.pickUpPoint = pickUpPoint;
-        this.destination = destination;
-        this.stops = stops;
-        this.timeStart = timeStart;
-        this.timeEnd = timeEnd;
-        this.notes = new ArrayList<>();
-        this.customers = new ArrayList<>();
-        this.chauffeur = chauffeur;
+    public Trip(Bus bus, Chauffeur chauffeur, Destination pickUpPoint, Destination destination, int distance,
+                LocalDate dateStart, String timeStart, LocalDate dateEnd, String timeEnd, String price) {
+       this.bus = bus;
+       this.chauffeur = chauffeur;
+       this.pickUpPoint = pickUpPoint;
+       this.destination = destination;
+       this.distance = distance;
+       String[] lineToken = timeStart.split(":");
+       int hours = Integer.parseInt(lineToken[0]);
+       int minutes = Integer.parseInt(lineToken[1]);
+       this.timeStart = new Date(dateStart.getYear()-1900, dateStart.getMonthValue(), dateStart.getDayOfMonth(), hours, minutes);
+       lineToken = timeEnd.split(":");
+       hours = Integer.parseInt(lineToken[0]);
+       minutes = Integer.parseInt(lineToken[1]);
+       this.timeEnd = new Date(dateEnd.getYear()-1900, dateEnd.getMonthValue(), dateEnd.getDayOfMonth(), hours, minutes);
+       this.price = Integer.parseInt(price);
+
 
     }
 
-    public Trip(Bus bus, Chauffeur chauffeur, Destination pickUpPoint, Destination destination, Date timeStart, Date timeEnd) {
-        this.bus = bus;
-        this.pickUpPoint = pickUpPoint;
-        this.destination = destination;
-        this.timeStart = timeStart;
-        this.timeEnd = timeEnd;
-        this.notes = new ArrayList<>();
-        this.customers = new ArrayList<>();
-        this.chauffeur = chauffeur;
+
+    public void setStops(DestinationList stops) {
+        this.stops = stops;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.isPrivate = true;
+        this.customer = customer;
     }
 
     public Bus getBus() {
@@ -59,7 +70,5 @@ public class Trip {
         notes.add(note);
     }
 
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
+
 }
